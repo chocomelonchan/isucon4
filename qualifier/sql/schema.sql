5b2f56fc4433f9c0d5fe9cd40f8ba810e10034fb
chocomelon <chocomelon@pixiv.com>
2015-09-23 23:38:23 +0900

    ipとsucceededにindexはってみる

--- qualifier/sql/schema.sql
+++ qualifier/sql/schema.sql
@@ -11,5 +11,6 @@ CREATE TABLE IF NOT EXISTS `login_log` (
   `user_id` int,
   `login` varchar(255) NOT NULL,
   `ip` varchar(255) NOT NULL,
-  `succeeded` tinyint NOT NULL
+  `succeeded` tinyint NOT NULL,
+   INDEX ip_succeeded(ip, succeeded)
 ) DEFAULT CHARSET=utf8;
