679bcd7e24d54b88f504f1b3c2907842c03d6d2f
chocomelon <chocomelon@pixiv.com>
2015-09-27 03:30:39 +0900

    fix syntax error

--- qualifier/sql/schema.sql
+++ qualifier/sql/schema.sql
@@ -11,8 +11,8 @@ CREATE TABLE IF NOT EXISTS `login_log` (
   `user_id` int,
   `login` varchar(255) NOT NULL,
   `ip` varchar(255) NOT NULL,
-  `succeeded` tinyint NOT NULL,
+  `succeeded` tinyint NOT NULL
 ) DEFAULT CHARSET=utf8;
 
-CREATE INDEX index_ip_succeeded(ip, succeeded);
-CREATE INDEX index_login_succeeded(login, succeeded);
+CREATE INDEX index_ip_succeeded ON login_log (ip, succeeded);
+CREATE INDEX index_login_succeeded ON login_log (login, succeeded);
